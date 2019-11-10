f147d64a3e03be8060e1a6f6e2af3e95c327a1dc
Mudassir Shaikhji <mudassirshaikhji@gmail.com>
2019-11-10 22:35:00 +0100

    code refactoring

--- src/test/java/com/assignment/celonis/SmokeSuiteTest.java
+++ src/test/java/com/assignment/celonis/SmokeSuiteTest.java
@@ -14,7 +14,6 @@ public class SmokeSuiteTest extends BaseTest {
 	
 	@Test
 	@DisplayName("When trying to login with invalid credentials, show an appropriate error message")
-//	@Disabled
 	void loginUsingInvalidCredentials() {
 		// Initialize
 		final String expectedErrorMessage = "Email or password are incorrect.";
@@ -23,13 +22,12 @@ public class SmokeSuiteTest extends BaseTest {
 		signIn.loginInvalidCredentials(invalidUsername, invalidUpassword);
 		String errorMessage = signIn.getErrorMessageInvalidLogin();
 		Utilities.takeScreenShot(driver, screenshotName);
-		// Assert or Verify
+		// Assert
 		assertEquals(expectedErrorMessage, errorMessage);
 	}
 
 	@Test
 	@DisplayName("Log in successful with valid credentials")
-//	@Disabled
 	void successfulLogin() {
 		// Initialize
 		final String expectedToolTipText = "Mudassir Shaikhji";
@@ -38,98 +36,112 @@ public class SmokeSuiteTest extends BaseTest {
 		homePage = signIn.login(username, password);
 		String avatarName = homePage.returnAvatarName();
 		Utilities.takeScreenShot(driver, screenshotName);
-		// Assert or verify
+		// Assert
 		assertEquals(expectedToolTipText, avatarName);
 	}
 
 	@Test
 	@DisplayName("Verifying that the forgot password page loads successfully")
-//	@Disabled
 	void checkIfForgotPasswordPageLoads() {
+		// Initialize
 		final String expectedToolTipText = "Forgot your password?";
 		final String screenshotName = "ForgotYourPassword";
+		// stimulate
 		forgotPasswordPage = signIn.goTOForgotPasswordPage();
 		String forgotPasswordText = forgotPasswordPage.pageLoadedString();
 		Utilities.takeScreenShot(driver, screenshotName);
+		// Assert
 		assertEquals(expectedToolTipText, forgotPasswordText);
 	}
 	
 	@Test
 	@DisplayName("Verifying that the Order To Cash page successfully loads")
-//	@Disabled
 	public void checkIfOrderToCashPageLoads() {
+		// Initialize
 		final String expectedText = "Order-to-Cash Analysis";
 		final String screenshotName = "OrderToCash";
+		// stimulate
 		homePage = signIn.login(username, password);
 		orderCashPage = homePage.navigateToOrderToCashPage();
 		String actualText = orderCashPage.returnOrderToCashAnalysisText();
 		Utilities.takeScreenShot(driver, screenshotName);
+		// Assert
 		assertEquals(expectedText, actualText);
 	}
 	
 	@Test
 	@DisplayName("Verifying that the Purchase To Pay page successfully loads")
-//	@Disabled
 	void checkIfPurchaseToPayPageLoads() {
+		// Initialize
 		final String expectedText = "Purchase-to-Pay Analysis";
 		final String screenshotName = "PurchaseToPay";
+		// stimulate
 		homePage = signIn.login(username, password);
 		ptpPage = homePage.navigateToPurchaseToPayPage();
 		String actualText = ptpPage.returnPurchaseToPayText();
 		Utilities.takeScreenShot(driver, screenshotName);
+		// Assert
 		assertEquals(expectedText, actualText);
 	}
 	
 	@Test
 	@DisplayName("Verifying if the Service now Ticketing page successfully loads")
-//	@Disabled
 	void checkIfServiceNowTicketingPageLoads() {
+		// Initialize
 		final String expectedText = "INCIDENT MANAGEMENT (TICKET STATUS)";
 		final String screenshotName = "ServiceNowTicketing";
+		// stimulate
 		homePage = signIn.login(username, password);
 		sntPage = homePage.navigateToServiceNowTicketingPage();
 		String actualText = sntPage.returnServiceNowTicketingText();
 		Utilities.takeScreenShot(driver, screenshotName);
+		// Assert
 		assertEquals(expectedText, actualText);
 	}
 	
 	@Test
 	@DisplayName("User is able to sign out")
-//	@Disabled
 	void signOut() {
+		// Initialize
 		final String screenshotName = "signOut";
+		// stimulate
 		homePage = signIn.login(username, password);
 		signedOutPage = homePage.signOut();
 		boolean isTextVisible = signedOutPage.isSignInToYourTeamTextVisible();
 		Utilities.takeScreenShot(driver, screenshotName);
+		// Assert
 		assertTrue(isTextVisible);		
 	}
 	
 	@Test
 	@DisplayName("Global search with input which does not return any result")
-//	@Disabled
 	void executeInvalidGlobalSearch() {
+		// Initialize
 		final String screenshotName = "InvalidGlobalSearch";
 		final String expectedText = "No results found";
 		final String searchText = "This search will not return any result";
+		// stimulate
 		homePage = signIn.login(username, password);
 		homePage.executeGlobalSearch(searchText);
 		String actualText = homePage.verifyNoResultsFoundText();
 		Utilities.takeScreenShot(driver, screenshotName);
+		// Assert
 		assertEquals(expectedText, actualText);		
 	}
 	
 	@Test
 	@DisplayName("Global search with valid input which returns some result")
-//	@Disabled
 	void executeValidGlobalSearch() {
+		// Initialize
 		final String screenshotName = "ValidGlobalSearch";
 		final String searchText = "ServiceNow Ticketing";
 		final String expectedText = "--> " + searchText;
+		// stimulate
 		homePage = signIn.login(username, password);
 		homePage.executeGlobalSearch(searchText);
 		String actualText = homePage.verifyValidResult();
 		Utilities.takeScreenShot(driver, screenshotName);
+		// Assert
 		assertEquals(expectedText, actualText);		
 	}
 	
